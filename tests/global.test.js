03b847c98be4edfa675e4527313106b114f3fd83
Marc-Olivier Laux <marc-olivier@bebe-au-naturel.com>
2021-03-14 22:14:17 +0100

    Enhance async tests

--- tests/global.test.js
+++ tests/global.test.js
@@ -11,12 +11,25 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const { PubSub } = graphqlSubscriptions
 
+const fixedStringWidth = (string, n) => string.length < n
+  ? string.concat(...Array.from({ length: n - string.length }, () => ' '))
+  : string
+
+const Breadcrumb = (widths) => (...args) => args.map((arg, i) => fixedStringWidth(arg, widths[i] ?? 0)).join(' > ')
+
 let request
 let subscribe
 const asyncIterators = {}
 let httpServer
 let wsClient
 const pubSub = new PubSub()
+const subscriptionsFiles = fs
+  .readdirSync(path.join(__dirname, 'gql', 'subscriptions'))
+
+const breadcrumb = Breadcrumb([12, 12, 15, 10])
+const subscriptions = subscriptionsFiles
+  .map((fileName) => fileName.split('.')[0])
+
 beforeAll(async () => {
   const server = await app()
 
@@ -25,9 +38,6 @@ beforeAll(async () => {
   await new Promise(resolve => httpServer.listen({ host: process.env.HOST, port: process.env.PORT }, () => resolve()))
   wsClient = getWsClient(`ws://${process.env.HOST}:${process.env.PORT}${server.apolloServer.subscriptionsPath}`)
   subscribe = subscribeFactory(getWsLink(wsClient))
-
-  const subscriptionsFiles = fs
-    .readdirSync(path.join(__dirname, 'gql', 'subscriptions'))
 
   for (const fileName of subscriptionsFiles) {
     const subscription = fs.readFileSync(path.join(__dirname, 'gql', 'subscriptions', fileName), 'utf8')
@@ -54,12 +64,50 @@ for (const requestFile of requestsFiles) {
   const [n, task] = requestFile.split('.')
   const query = fs.readFileSync(path.join(__dirname, 'gql', requestFile), 'utf8')
   const expected = JSON.parse(fs.readFileSync(path.join(__dirname, 'json', `${n}.${task}.json`), 'utf8'))
-  let publications
-  test(`${task} (${n}) > query > response`, async () => {
-    publications = Promise.all(Object.keys(expected.publications ?? []).map((subscription) => new Promise((resolve) => {
-      return asyncIterators[subscription].next().then((response) => resolve([response.value, expected.publications[subscription]]))
-    }
-    )))
+  let expectedPublicationsPromises
+  let expectedPublications
+  let unexpectedPublicationsPromises = []
+  let unexpectedPublications
+  expected.publications = expected.publications ?? {}
+  test(breadcrumb(`${fixedStringWidth(`[${n}]`, 5)} ${task}`, 'query', 'default', 'response'), async () => {
+    expectedPublicationsPromises = Object.keys(expected.publications).map((subscription) => new Promise((resolve) => {
+      const first = setTimeout(() => {
+        asyncIterators[subscription].return()
+        resolve({ subscription, response: {}, expected: expected.publications[subscription] })
+      }, 200)
+      asyncIterators[subscription].next().then((response) => {
+        clearTimeout(first)
+        unexpectedPublicationsPromises.push(new Promise((resolve) => {
+          const second = setTimeout(() => {
+            asyncIterators[subscription].return()
+            resolve({ subscription, response: {} })
+          }, 200)
+          asyncIterators[subscription].next()
+            .then((response) => {
+              clearTimeout(second)
+              asyncIterators[subscription].return()
+              resolve({ subscription, response: response.value })
+            })
+        }))
+        clearTimeout(first)
+        resolve({ subscription, response: response.value, expected: expected.publications[subscription] })
+      })
+    }))
+
+    unexpectedPublicationsPromises = Object.keys(asyncIterators)
+      .filter((subscription) => !(subscription in expected.publications))
+      .map((subscription) => new Promise((resolve) => {
+        const first = setTimeout(() => {
+          asyncIterators[subscription].return()
+          resolve({ subscription, response: {} })
+        }, 200)
+        asyncIterators[subscription].next()
+          .then((response) => {
+            clearTimeout(first)
+            asyncIterators[subscription].return()
+            resolve({ subscription, response: response.value })
+          })
+      }))
 
     const result = await request
       .post(process.env.API_APOLLO_PATH)
@@ -69,34 +117,29 @@ for (const requestFile of requestsFiles) {
       .expect(200)
     expect(result.body).toBeInstanceOf(Object)
     expect(result.body).toEqual(expected.response)
-  })
 
-  let i = 0
-  for (const publication in expected.publications) {
-    test(`${task} (${n}) > subscription > ${publication} > response`, async () => {
-      const [response, expected] = (await publications)[i++]
-      expect(response).toEqual(expected)
-    })
-  }
-  
+    expectedPublications = (await Promise.all(expectedPublicationsPromises))
+      .reduce((o, { subscription, ...rest }) => ({ ...o, [subscription]: { subscription, ...rest } }), {})
 
-  test(`${task} (${n}) > subscription > all > have no additional publication`, async () => {
-    const noMore = await new Promise((resolve) => {
-      setTimeout(() => Object.keys(asyncIterators).map((subscription) => {
-        asyncIterators[subscription].return()
-        return resolve(true)
-      }), 200)
-
-      Promise.all(Object.keys(asyncIterators).map((subscription) => new Promise((resolve) =>
-        asyncIterators[subscription].next().then((response) => resolve(response))
-      ))).then(() => resolve(false))
-    })
-
-    expect(noMore).toEqual(true)
+    unexpectedPublications = (await Promise.all(unexpectedPublicationsPromises))
+      .reduce((o, { subscription, ...rest }) => ({ ...o, [subscription]: { subscription, ...rest } }), {})
 
     for (const name in asyncIterators) {
       asyncIterators[name] = pubSub.asyncIterator(name)
     }
   })
-   
+
+  for (const publication in expected.publications) {
+    test(breadcrumb(`${fixedStringWidth(`[${n}]`, 5)} ${task}`, 'subscription', publication, 'response'), () => {
+      const { expected, response } = expectedPublications[publication]
+      expect(response).toEqual(expected)
+    })
+  }
+
+  for (const publication of subscriptions) {
+    test(breadcrumb(`${fixedStringWidth(`[${n}]`, 5)} ${task}`, 'subscription', publication, 'no more'), () => {
+      const { response } = unexpectedPublications[publication]
+      expect(response).toEqual({})
+    })
+  }
 }
